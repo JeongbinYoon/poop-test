c0b34e58fa2e19df572b4c4a81dfce2298bc970d
JeongbinYoon <yon980824@gmail.com>
2024-06-01 14:47:18 +0900

    root layout session provider 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,17 @@ import { Layout } from 'components/layout';
 import { Toaster } from 'react-hot-toast';
 import { type PropsWithChildren } from 'react';
 import '@/styles/globals.css';
-import AuthProvider from './auth/provider';
 
 const MyApp = ({ children }: PropsWithChildren) => {
   return (
     <html suppressHydrationWarning>
       <body>
-        <AuthProvider>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Layout>
-              {children}
-              <Toaster />
-            </Layout>
-          </ThemeProvider>
-        </AuthProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Layout>
+            {children}
+            <Toaster />
+          </Layout>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,30 +1,33 @@
-"use client";
-import React from "react";
-import Header from "components/layout/header";
-import Sidebar from "components/layout/sidebar";
+'use client';
+import React from 'react';
+import Header from 'components/layout/header';
+import Sidebar from 'components/layout/sidebar';
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { usePathname } from "next/navigation";
+} from '@/components/ui/resizable';
+import { usePathname } from 'next/navigation';
+import AuthProvider from '@/app/auth/provider';
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   return (
     <>
-      <Header />
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="flex h-screen border-collapse overflow-hidden"
-      >
-        {!pathname.includes("auth") && <Sidebar />}
-        <ResizableHandle withHandle className="hidden md:flex" />
-        <ResizablePanel defaultSize={80}>
-          <main className="flex-1 overflow-y-auto overflow-x-hidden bg-secondary/10 pb-1 pt-16">
-            {children}
-          </main>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      <AuthProvider>
+        <Header />
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="flex h-screen border-collapse overflow-hidden"
+        >
+          {!pathname.includes('auth') && <Sidebar />}
+          <ResizableHandle withHandle className="hidden md:flex" />
+          <ResizablePanel defaultSize={80}>
+            <main className="flex-1 overflow-y-auto overflow-x-hidden bg-secondary/10 pb-1 pt-16">
+              {children}
+            </main>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </AuthProvider>
     </>
   );
 };
